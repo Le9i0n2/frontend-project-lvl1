1998a7091ee9cb46f3d3253e9fd358e34edbd383
Vasiliy Dontzov <djchevicheis@gmail.com>
2022-02-27 20:41:10 +0300

    Refactor folder structure in bin/

--- bin/games/brain-prime.js
+++ bin/games/brain-prime.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import helloUser from '../src/cli.js';
-import primeGame from '../src/index.js';
+import helloUser from '../../src/cli.js';
+import primeGame from '../../src/index.js';
 
 console.log('Welcome to the Brain Games!');
 const userName = helloUser();
