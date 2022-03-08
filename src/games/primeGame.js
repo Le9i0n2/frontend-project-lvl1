425e5ded2e243988fec5157dd4e76941b369b40b
Vasiliy Dontzov <djchevicheis@gmail.com>
2022-03-08 16:49:30 +0300

    Fix primeGame

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -1,18 +1,22 @@
 // Logic for brain-prime game
 
-import index from '../index.js';
+import playGame, { gameRounds } from '../index.js';
 import getRandomInt from '../getRandomInt.js';
 
 // Check if a number is prime. Return 'yes' or 'no'
 const isPrime = (num) => {
+  if (num === 1) {
+    return true;
+  }
+
   const halfOfNum = Math.ceil(num / 2);
   for (let i = 2; i <= halfOfNum; i += 1) {
     if (num % i === 0) {
-      return 'no';
+      return false;
     }
   }
 
-  return 'yes';
+  return true;
 };
 
 export default () => {
@@ -20,12 +24,12 @@ export default () => {
   const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
   const gameData = [];
   // Getting set of game data (three pairs question-correctAnswer)
-  for (let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < gameRounds; i += 1) {
     const question = getRandomInt(2, 100);
-    const correctAnswer = isPrime(question);
+    const correctAnswer = isPrime(question) ? 'yes' : 'no';
     gameData.push([question, correctAnswer]);
   }
 
   // Start the game
-  index(rules, gameData);
+  playGame(rules, gameData);
 };
