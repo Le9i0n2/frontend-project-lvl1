b0046fb93aca453b6098465c12fbe9c5d14a17b5
Vasiliy Dontzov <djchevicheis@gmail.com>
2022-03-08 16:34:58 +0300

    Fix calcGame

--- src/games/calcGame.js
+++ src/games/calcGame.js
@@ -1,6 +1,6 @@
 // Logic for brain-calc game
 
-import index from '../index.js';
+import playGame, { gameRounds } from '../index.js';
 import getRandomInt from '../getRandomInt.js';
 
 // Provides a map of mathematical operations for brain-calc game
@@ -10,26 +10,23 @@ const calcMap = {
   '*': (n1, n2) => n1 * n2,
 };
 
-// Return random math sign from these three: {+, -, *}
-const getRandomMathSign = () => {
-  const mathSigns = ['+', '-', '*'];
-  return mathSigns[Math.floor(Math.random() * mathSigns.length)];
-};
+// Array of math signs
+const mathSigns = ['+', '-', '*'];
 
 export default () => {
   // Set rules for the game
   const rules = 'What is the result of the expression?';
   const gameData = [];
   // Getting set of game data (three pairs question-correctAnswer)
-  for (let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < gameRounds; i += 1) {
     const num1 = getRandomInt(1, 50);
     const num2 = getRandomInt(1, 10);
-    const sign = getRandomMathSign();
+    const sign = mathSigns[getRandomInt(0, 2)];
     const question = `${num1} ${sign} ${num2}`;
     const correctAnswer = calcMap[sign](num1, num2);
     gameData.push([question, correctAnswer]);
   }
 
   // Start the game
-  index(rules, gameData);
+  playGame(rules, gameData);
 };
